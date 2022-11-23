1112e9eb199ce342322da1f0add37b1c769d7d7f
mitsui3point <rladbsgh0331@gmail.com>
2022-11-23 17:16:40 +0900

    refactor($example, $main): modify - modify extract example functions

---
+++ src/main/java/hellojpa/JpaCRUD.java
@@ -0,0 +1,62 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class JpaCRUD {
+
+    /**
+     * 회원 엔티티 목록 조회
+     * JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
+     */
+    public static List<Member> selectMembersByNameWithPaging(EntityManager em, int start, int end, String searchNameCondition) {
+        List<Member> members = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", searchNameCondition)
+                .setFirstResult(start)
+                .setMaxResults(end)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member.getName() = " + member.getName());
+        }
+
+        return members;
+    }
+
+    /**
+     * 회원 엔티티 수정
+     */
+    public static void updateMemberName(EntityManager em, Long id) {
+        Member member = selectMember(em, id);
+        member.setName("helloJpa");
+    }
+
+    /**
+     * 회원 엔티티 삭제
+     */
+    public static void deleteMember(EntityManager em, Long id) {
+        Member member = selectMember(em, id);
+        em.remove(member);
+    }
+
+    /**
+     * 회원 엔티티 단건 조회
+     */
+    public static Member selectMember(EntityManager em, long id) {
+        Member member = em.find(Member.class, id);
+        System.out.println("member = " + member);
+
+        return member;
+    }
+
+    /**
+     * 회원 엔티티 등록
+     */
+    public static void insertMember(EntityManager em, long id, String name) {
+        Member member = new Member();
+        member.setId(id);
+        member.setName(name);
+
+        em.persist(member);
+    }
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,7 +16,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> members = selectMembersByNameWithPaging(em, 0, 3, "helloJpa");
+            //JPA code
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -24,60 +24,5 @@ public class JpaMain {
             em.close();
         }
         emf.close();
-    }
-
-    /**
-     * 회원 엔티티 목록 조회
-     * JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
-     */
-    private static List<Member> selectMembersByNameWithPaging(EntityManager em, int start, int end, String searchNameCondition) {
-        List<Member> members = em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", searchNameCondition)
-                .setFirstResult(start)
-                .setMaxResults(end)
-                .getResultList();
-
-        for (Member member : members) {
-            System.out.println("member.getName() = " + member.getName());
-        }
-
-        return members;
-    }
-
-    /**
-     * 회원 엔티티 수정
-     */
-    private static void updateMemberName(EntityManager em, Long id) {
-        Member member = selectMember(em, id);
-        member.setName("helloJpa");
-    }
-
-    /**
-     * 회원 엔티티 삭제
-     */
-    private static void deleteMember(EntityManager em, Long id) {
-        Member member = selectMember(em, id);
-        em.remove(member);
-    }
-
-    /**
-     * 회원 엔티티 단건 조회
-     */
-    private static Member selectMember(EntityManager em, long id) {
-        Member member = em.find(Member.class, id);
-        System.out.println("member = " + member);
-
-        return member;
-    }
-
-    /**
-     * 회원 엔티티 등록
-     */
-    private static void insertMember(EntityManager em, long id, String name) {
-        Member member = new Member();
-        member.setId(id);
-        member.setName(name);
-
-        em.persist(member);
     }
 }
