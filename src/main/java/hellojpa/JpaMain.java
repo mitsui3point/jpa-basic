f511a9a0c55b26f1d36b0c1426223bf3ce5a5081
mitsui3point <rladbsgh0331@gmail.com>
2022-11-24 19:41:43 +0900

    feat($entity): modify - modify 자동 할당: @GeneratedValue strategy = SEQUENCE allocationSize = 50 성능 개선 ==================== Hibernate:     call next value for MEMBER_SEQ Hibernate:     call next value for MEMBER_SEQ member1 = hellojpa.entity.Member@58dea0a5 member2 = hellojpa.entity.Member@3bb9efbc member3 = hellojpa.entity.Member@1cefc4b3 member4 = hellojpa.entity.Member@2b27cc70 member5 = hellojpa.entity.Member@6f6a7463 ====================

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -10,11 +10,35 @@ import static hellojpa.context.JpaPersistenceContext.create;
 public class JpaMain {
     public static void main(String[] args) {
         create(em -> {
-            Member member = new Member();
-            member.setUsername("usernameA");
+            Member member1 = new Member();
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            Member member4 = new Member();
+            member4.setUsername("D");
+
+            Member member5 = new Member();
+            member5.setUsername("E");
+
             System.out.println("====================");
-            em.persist(member);
-            System.out.println("member.getId() = " + member.getId());
+
+            em.persist(member1);//call next value for MEMBER_SEQ 1, call next value for MEMBER_SEQ 51
+            em.persist(member2);//memory
+            em.persist(member3);//memory
+            em.persist(member4);//memory
+            em.persist(member5);//memory
+
+            System.out.println("member1 = " + member1);
+            System.out.println("member2 = " + member2);
+            System.out.println("member3 = " + member3);
+            System.out.println("member4 = " + member4);
+            System.out.println("member5 = " + member5);
+
             System.out.println("====================");
         });
     }
