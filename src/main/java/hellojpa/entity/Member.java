729fbc95710d4f6159fb09778814058aaa309b8c
mitsui3point <rladbsgh0331@gmail.com>
2022-11-24 18:41:09 +0900

    feat($entity): modify - modify 자동 할당: @GeneratedValue strategy = SEQUENCE

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Member {
     @Id
     @GeneratedValue(
-            strategy = GenerationType.IDENTITY
+            strategy = GenerationType.SEQUENCE
     )
     private Long id;
 
