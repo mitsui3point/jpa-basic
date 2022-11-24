ea5bd7df75180a34337a9ac155fd144718d389c5
mitsui3point <rladbsgh0331@gmail.com>
2022-11-24 18:21:58 +0900

    feat($entity, $constants): modify - modify 직접 할당: @Id만 사용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,14 +11,9 @@ public class JpaMain {
     public static void main(String[] args) {
         create(em -> {
             Member member = new Member();
-            member.setId(3L);
-            member.setUsername("member3");
-            member.setRoleType(RoleType.GUEST);
+            member.setId("ID_A");
+            member.setUsername("usernameA");
             em.persist(member);
-            Member roleTypeMember = em.find(Member.class, member.getId());
-            RoleType roleType = member.getRoleType();
-            System.out.println("roleTypeMember = " + roleTypeMember.getRoleType());
-//            JpaCRUD.selectMember(em, 160L);
         });
     }
 }
