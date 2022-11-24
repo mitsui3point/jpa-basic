ddbe05d92d2ed1146c384217d8d5bf417c8412db
mitsui3point <rladbsgh0331@gmail.com>
2022-11-24 18:40:44 +0900

    feat($entity): modify - modify 자동 할당: @GeneratedValue strategy = IDENTITY

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -3,12 +3,10 @@ package hellojpa.entity;
 import javax.persistence.*;
 
 @Entity
-//테이블 컬럼 unique 제약조건 @Column unique = true 대신 보편적으로 사용한다.
-//@Table(uniqueConstraints = { @UniqueConstraint(name = "uk_name", columnNames = "name")})
 public class Member {
     @Id
     @GeneratedValue(
-            strategy = GenerationType.AUTO//DB 방언에 맞춰 자동생성
+            strategy = GenerationType.IDENTITY
     )
     private Long id;
 
