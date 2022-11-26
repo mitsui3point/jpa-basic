3e7712e38aa9038115aa40437841fb145aa1d23d
mitsui3point <rladbsgh0331@gmail.com>
2022-11-26 16:23:52 +0900

    feat($jpa, $entity): add - add $entity 양방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,8 @@ package hellojpa;
 import hellojpa.entity.Member;
 import hellojpa.entity.Team;
 
+import java.util.List;
+
 import static hellojpa.context.JpaPersistenceContext.create;
 
 public class JpaMain {
@@ -13,29 +15,24 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setName("memberA");
-            member.setTeam(team);
-            em.persist(member);
+            Member memberA = new Member();
+            memberA.setName("memberA");
+            memberA.setTeam(team);
+            em.persist(memberA);
+
+            Member memberB = new Member();
+            memberB.setName("memberB");
+            memberB.setTeam(team);
+            em.persist(memberB);
 
             em.flush();//1차캐시 비우고 다시 조회하기 위한 목적
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());//member 1차캐시에서 제거됨, 1차캐시 재등록
-
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
-
-            Team newTeam = new Team();
-            newTeam.setName("newTeam");
-            em.persist(newTeam);
-
-            findMember.setTeam(newTeam);
-
-            Team findAfterSetTeam = em.find(Member.class,
-                            member.getId())
-                    .getTeam();
-            System.out.println("findAfterSetTeam = " + findAfterSetTeam.getName());
+            Member findMember = em.find(Member.class, memberA.getId());
+            List<Member> members = findMember.getTeam().getMembers();//member -> team -> member ; 양방향 연관관계
+            for (Member m : members) {
+                System.out.println("member = " + m.getName());
+            }
         });
     }
 }
--- src/main/java/hellojpa/entity/Team.java
+++ src/main/java/hellojpa/entity/Team.java
@@ -1,9 +1,8 @@
 package hellojpa.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -15,6 +14,9 @@ public class Team {
 
     @Column(name = "TEAM_NAME")
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -30,5 +32,9 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return this.members;
     }
 }
