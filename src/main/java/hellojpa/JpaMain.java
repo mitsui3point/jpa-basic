9af9f56847415f458ea6d65a52a1725de134daef
mitsui3point <rladbsgh0331@gmail.com>
2022-12-01 01:41:00 +0900

    feat($jpaMain): update - update 프록시와 즉시로딩 주의 • 가급적 지연 로딩만 사용(특히 실무에서) • 즉시 로딩을 적용하면 예상하지 못한 SQL이 발생 • 즉시 로딩은 JPQL에서 N+1 문제를 일으킨다. • @ManyToOne, @OneToOne은 기본이 즉시 로딩  -> LAZY로 설정 • @OneToMany, @ManyToMany는 기본이 지연 로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,32 +3,44 @@ package hellojpa;
 import hellojpa.entity.Member;
 import hellojpa.entity.Team;
 
+import java.util.List;
+
 import static hellojpa.context.JpaPersistenceContext.create;
 
 public class JpaMain {
     public static void main(String[] args) {
         create((em, emf) -> {
 
-            Team team = new Team();
-            team.setName("team1");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("team1");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setName("member1");
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("team2");
+            em.persist(teamB);
+
+            Member memberA = new Member();
+            memberA.setName("member1");
+            memberA.setTeam(teamA);
+            em.persist(memberA);
+
+            Member memberB = new Member();
+            memberB.setName("member2");
+            memberB.setTeam(teamB);
+            em.persist(memberB);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            //Member @ManyToOne hellojpa.entity.Team
-            //Member @ManyToOne(fetch = FetchType.LAZY) hellojpa.entity.Team$HibernateProxy$biOUnHhi
-            System.out.println("findMember.getTeam().getClass() = " + findMember.getTeam().getClass());
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
-            System.out.println("======================");
-            findMember.getTeam().getName();
-            System.out.println("======================");
+            //SQL: select * from member,
+            //SQL: select * from team where TEAM_ID = xxx,
+            //  EAGER 이므로 team 도 필요함.
+            //  member 의 조회된 건수만큼의 team 테이블 쿼리가 나간다.
+
+
         });
     }
 }
