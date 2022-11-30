5f584338bdfab1c1160b3e2bd5b445d9faf718ff
mitsui3point <rladbsgh0331@gmail.com>
2022-11-30 17:09:33 +0900

    feat($jpaMain, $test, $domain): update - update 프록시의 특징 • 영속성 컨텍스트에 찾는 엔티티가 이미  em.getReference() 로 조회가 되었으면, em.find()로 호출해도 proxy 객체 반환

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,10 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member findMember1 = em.find(Member.class, member.getId());//Member
+            Member findMember1 = em.getReference(Member.class, member.getId());//Proxy
             System.out.println("findMember1 = " + findMember1.getClass());
 
-            Member findMember2 = em.getReference(Member.class, member.getId());//expected Proxy but Member
+            Member findMember2 = em.find(Member.class, member.getId());//expected Member but Proxy
             System.out.println("findMember2 = " + findMember2.getClass());
 
             System.out.println("findMember1 == findMember2 : " + (findMember1 == findMember2));// == 비교 메커니즘을 맞추기 위해 타입을 맞춤
