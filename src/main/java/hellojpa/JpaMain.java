e0daede56328c14842024f4ff1cf7d3ccf76ecca
mitsui3point <rladbsgh0331@gmail.com>
2022-11-30 16:51:50 +0900

    feat($jpaMain, $test, $domain): update - update 프록시의 특징 프록시 객체는 원본 엔티티를 상속받음, 따라서 타입 체크시 주의해야함 (== 비 교 실패, 대신 instance of 사용)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,32 +1,38 @@
 package hellojpa;
 
 import hellojpa.entity.Member;
-import hellojpa.entity.item.Item;
-import hellojpa.entity.item.Movie;
-
-import java.time.LocalDateTime;
 
 import static hellojpa.context.JpaPersistenceContext.create;
 
 public class JpaMain {
     public static void main(String[] args) {
         create(em -> {
-            Member member = new Member();
-            member.setName("name");
+            Member member1 = new Member();
+            member1.setName("member1");
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setName("member1");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member findMember = em.find(Member.class, member.getId());//1.데이터베이스를 통해서 실제 엔티티 객체 조회
-            Member findMember = em.getReference(Member.class, member.getId());//2.데이터배이스 조회를 미루는 가짜(프록시) 엔티티 객체 조회
+            Member findMember1 = em.find(Member.class, member1.getId());
+            Member findMember2 = em.getReference(Member.class, member2.getId());
 
-            //1.hellojpa.entity.Member
-            //2.hellojpa.entity.Member$HibernateProxy$FuPunnFY
-            System.out.println("before  findMember.getClass() = " + findMember.getClass());
-            System.out.println("findMember.name = " + findMember.getName());
-            System.out.println("after   findMember.getClass() = " + findMember.getClass());
+            externalLogic(findMember1, findMember2);
         });
+    }
+
+    private static void externalLogic(Member member1, Member member2) {
+        System.out.println("member1 = " + member1.getClass());
+        System.out.println("member2 = " + member2.getClass());
+        boolean result = member1.getClass() == member2.getClass();
+        System.out.println("member1 == member2 = " + result);
+        boolean result1 = member1 instanceof Member;
+        boolean result2 = member2 instanceof Member;
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
     }
 }
