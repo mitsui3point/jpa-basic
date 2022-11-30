ea0f663d298d4dd5783a042bb0fb95127eac22b8
mitsui3point <rladbsgh0331@gmail.com>
2022-11-30 17:42:16 +0900

    feat($jpaMain): update - update 프록시 확인 프록시 강제 초기화 org.hibernate.Hibernate.initialize(entity)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import hellojpa.context.JpaCode;
 import hellojpa.entity.Member;
+import org.hibernate.Hibernate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -20,6 +21,9 @@ public class JpaMain {
 
             Member findMember = em.getReference(Member.class, member.getId());//Proxy
             System.out.println("findMember1 = " + findMember.getClass());//hellojpa.entity.Member$HibernateProxy$11wPBqYD
+
+//            findMember.getName();//강제 초기화
+            Hibernate.initialize(findMember);//강제 초기화
         });
     }
 }
