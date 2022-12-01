f936292738e2367999e4c3bd0c21dd78f06e9915
mitsui3point <rladbsgh0331@gmail.com>
2022-12-01 20:40:20 +0900

    feat($jpaMain, $domain): update - update 객체 타입의 한계 보완 - 불변 객체 불변 객체를 만든 후 복사방법
    불변이라는 작은 제약으로 부작용이라는 큰 재앙을 막을 수 있다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,6 +12,7 @@ public class JpaMain {
     public static void main(String[] args) {
         create((em, emf) -> {
             Address address = new Address("city", "address", "zipcode");
+            Address newAddress = new Address("newcity", address.getAddress(), address.getZipcode());//값 타입 복사
 
             Member member1 = new Member();
             member1.setName("member1");
@@ -20,13 +21,11 @@ public class JpaMain {
 
             Member member2 = new Member();
             member2.setName("member2");
-            member2.setHomeAddress(address);
+            member2.setHomeAddress(newAddress);
             em.persist(member2);
 
             em.flush();
             em.clear();
-
-//            member1.getHomeAddress().setCity("newCity"); //불변 객체; 객체 변경 불가, 컴파일 레벨 오류
 
             //member1, member2 의도된 값이 입력됨
             System.out.println("member1.getHomeAddress().getCity() = " + member1.getHomeAddress().getCity());
