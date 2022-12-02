4f75dc67a1e3ec672414a24d0425aa3bd34ccf38
mitsui3point <rladbsgh0331@gmail.com>
2022-12-02 23:05:54 +0900

    feat($domain): update - update JPQL • JPA를 사용하면 엔티티 객체를 중심으로 개발 • 문제는 검색 쿼리 • 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색 • 모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능 • 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요
    JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
    • SQL과 문법 유사, SELECT, FROM, WHERE, GROUP BY, HAVING, JOIN 지원
    • JPQL은 엔티티 객체를 대상으로 쿼리
    • SQL은 데이터베이스 테이블을 대상으로 쿼리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,51 +1,21 @@
 package hellojpa;
 
-import hellojpa.entity.AddressEntity;
 import hellojpa.entity.Member;
-import hellojpa.entity.embeddable.Address;
 
 import java.util.List;
-import java.util.Set;
 
 import static hellojpa.context.JpaPersistenceContext.create;
 
 public class JpaMain {
     public static void main(String[] args) {
         create((em, emf) -> {
-            Member member = new Member("member1", new Address("homeCity", "street", "10000"));
-
-            //값타입 Address -> AddressEntity 로 승급
-            AddressEntity old1City = new AddressEntity("old1City", "street", "10000");
-            member.getAddressHistory().add(old1City);
-            AddressEntity old2City = new AddressEntity("old2City", "street", "10000");
-            member.getAddressHistory().add(old2City);
-
-            //값타입 사용시기; [치킨, 피자, 족발] 체크박스
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("================ start ================");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            //homeCity -> newCity
-////            findMember.getHomeAddress().setCity("newCity");
-//            Address original = findMember.getHomeAddress();
-//            findMember.putHomeAddress(new Address("newCity", original.getStreet(), original.getZipcode()));
-//
-//            //치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-            //old1City -> newCity
-            AddressEntity findAddress = em.find(AddressEntity.class, old1City.getId());
-            findAddress.setAddress(new Address("newCity", findAddress.getAddress().getStreet(), findAddress.getAddress().getZipcode()));
-
+            List<Member> members = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'",
+                            Member.class)
+                    .getResultList();
+            for (Member member : members) {
+                System.out.println("member = " + member.getId());
+            }
         });
     }
 }
