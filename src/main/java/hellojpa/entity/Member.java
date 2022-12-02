21e7aa897927e272147ca4b949efbc6d7965f873
mitsui3point <rladbsgh0331@gmail.com>
2022-12-02 14:32:12 +0900

    feat($domain): update - update 값 타입 컬렉션 • 값 타입을 하나 이상 저장할 때 사용 • @ElementCollection, @CollectionTable 사용 • 데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없다. • 컬렉션을 저장하기 위한 별도의 테이블이 필요함

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -1,15 +1,15 @@
 package hellojpa.entity;
 
-import hellojpa.entity.base.BaseEntity;
 import hellojpa.entity.embeddable.Address;
-import hellojpa.entity.embeddable.Period;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id
     @GeneratedValue
@@ -19,26 +19,20 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
-
     @Embedded
-    private Period workPeriod;//Period;기간
+    private Address homeAddress;
 
-    @Embedded
-    private Address homeAddress;//Address;주소
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
 
-    @Embedded
-    @AttributeOverrides(value = {
-            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
-            @AttributeOverride(name = "address", column = @Column(name = "WORK_ADDRESS")),
-            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Member() {
     }
@@ -47,39 +41,8 @@ public class Member extends BaseEntity {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
-    public Address getHomeAddress() {
-        return homeAddress;
-    }
-
-    public void setHomeAddress(Address homeAddress) {
-        this.homeAddress = homeAddress;
-    }
 }
