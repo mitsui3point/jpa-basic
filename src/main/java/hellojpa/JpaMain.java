9014298226088fb0a3e39830b7ea5454826cc7ae
mitsui3point <rladbsgh0331@gmail.com>
2022-12-02 23:27:21 +0900

    feat($jpaMain): update - update Criteria • 문자가 아닌 자바코드로 JPQL을 작성할 수 있음 • JPQL 빌더 역할 • JPA 공식 기능 • 단점: 너무 복잡하고 실용성이 없다. • Criteria 대신에 QueryDSL 사용 권장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,10 @@ package hellojpa;
 
 import hellojpa.entity.Member;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import java.util.List;
 
 import static hellojpa.context.JpaPersistenceContext.create;
@@ -9,12 +13,25 @@ import static hellojpa.context.JpaPersistenceContext.create;
 public class JpaMain {
     public static void main(String[] args) {
         create((em, emf) -> {
-            List<Member> members = em.createQuery(
-                    "select m from Member m where m.name like '%kim%'",
-                            Member.class)
-                    .getResultList();
+            //criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
+
+            //쿼리 생성
+            CriteriaQuery<Member> cq = query.select(m);
+
+            //동적 쿼리
+            String username = "kim";
+            if (username != null) {
+                cq = cq.where(cb.equal(m.get("name"), username));
+            }
+
+            List<Member> members = em.createQuery(cq).getResultList();
             for (Member member : members) {
-                System.out.println("member = " + member.getId());
+                System.out.println("member = " + member);
             }
         });
     }
